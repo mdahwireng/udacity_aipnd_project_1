b72fafa8ae65ded68a30501a8f242b7cbd14bb16
kaaymike <kaaymike@hotmail.co.uk>
2020-01-29 18:00:13 +0000

    updating the revision date

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: M. D. Ahwireng
 # DATE CREATED: 27/01/2020                                 
-# REVISED DATE: 
+# REVISED DATE: 29/01/2020
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
